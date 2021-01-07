f8b40566c54e711bae1b49212a78dce21d5d8dff
Oleh Podolian <olegvanich@gmail.com>
2021-01-07 21:22:45 +0200

    first commit

---
+++ src/test/java/oleh/podolian/osbb/OsbbApplicationTests.java
@@ -0,0 +1,13 @@
+package oleh.podolian.osbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
