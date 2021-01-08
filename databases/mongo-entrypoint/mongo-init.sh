7e6bf0db69fdf1835e7484ad9fed9d07bc083db7
Oleh Podolian <olegvanich@gmail.com>
2021-01-08 18:59:46 +0200

    added props

--- databases/mongo-entrypoint/mongo-init.sh
+++ databases/mongo-entrypoint/mongo-init.sh
@@ -4,7 +4,12 @@ echo -e "\e[32mCreating mongo users...\e[0m"
 mongo -u ${MONGO_INITDB_ROOT_USERNAME} \
       -p ${MONGO_INITDB_ROOT_PASSWORD} \
       --authenticationDatabase ${AUTH_DB} \
-      --eval "defaultdb = db.getSiblingDB('${DB_NAME}'); defaultdb.createUser({user: '${DB_USER}', pwd: '${DB_PASS}', roles: [{role: 'readWrite', db: '${DB_NAME}'}]});"
+      --eval "
+              defaultdb = db.getSiblingDB('${DB_NAME}');
+              defaultdb.createUser({user: '${DB_USER}', pwd: '${DB_PASS}', roles: [{role: 'readWrite', db: '${DB_NAME}'}]});
+              admindb = db.getSiblingDB('admin');
+              admindb.createUser({user: '${MONITORING_USER}', pwd: '${MONITORING_PASS}', roles: [{role: 'clusterMonitor', db: 'admin'}]});
+              "
 
 echo -e "\e[32mCreated user '\e[4m${DB_USER}\e[24m' for database '\e[4m${DB_NAME}\e[24m'\e[0m"
 
